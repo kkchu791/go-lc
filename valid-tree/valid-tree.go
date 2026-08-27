21e521b089eb3217316faac345ce28cd75c86ac4
Kirk Chu <kkchu791@gmail.com>
2026-08-27 14:54:41 -0700

    valid graph 2nd iter

--- valid-tree/valid-tree.go
+++ valid-tree/valid-tree.go
@@ -2,8 +2,7 @@ package validtree
 
 func validTree(n int, edges [][]int) bool {
 	al := make(map[int][]int, n)
-	visiting := make(map[int]struct{})
-	visited := make(map[int]struct{})
+	visit := make(map[int]struct{})
 
 	for _, edge := range edges {
 		al[edge[0]] = append(al[edge[0]], edge[1])
@@ -13,26 +12,22 @@ func validTree(n int, edges [][]int) bool {
 	var hasCycle func(int, int) bool
 
 	hasCycle = func(node, parent int) bool {
-		if _, exists := visiting[node]; exists {
+		if _, exists := visit[node]; exists {
 			return true
 		}
 
-		if _, exists := visited[node]; exists {
-			return false
-		}
-
-		visiting[node] = struct{}{}
+		visit[node] = struct{}{}
 
 		for _, neighbor := range al[node] {
-			if parent != neighbor {
-				if hasCycle(neighbor, node) {
-					return true
-				}
+			if parent == neighbor {
+				continue
+			}
+
+			if hasCycle(neighbor, node) {
+				return true
 			}
 		}
 
-		visited[node] = struct{}{}
-		delete(visiting, node)
 		return false
 	}
 
@@ -40,5 +35,5 @@ func validTree(n int, edges [][]int) bool {
 		return false
 	}
 
-	return n == len(visited)
+	return n == len(visit)
 }
