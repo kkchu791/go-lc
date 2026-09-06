959bfde0b9889670493fbb7109769f1d505db50e
Kirk Chu <kkchu791@gmail.com>
2026-09-06 15:04:16 -0700

    add part II topo sort for alien dict

--- alien-dictionary/alien-dict.go
+++ alien-dictionary/alien-dict.go
@@ -1,11 +1,25 @@
 package aliendictionary
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func alienDict(words []string) string {
 
 	set := make(map[rune]struct{})
 	al := make(map[rune][]rune)
+
+	// because I loop with the expectation of 2 words i start at the second index
+	// but for words having just 1 word, i still want to add it to the set
+	// It'll just be vertices with no edges, no need for matching logic
+	if len(words) == 1 {
+		a := words[0]
+
+		for _, r := range a {
+			set[r] = struct{}{}
+		}
+	}
 
 outer:
 	for i := 1; i < len(words); i++ {
@@ -27,7 +41,6 @@ outer:
 		j := 0
 
 		for j < len(runeA) && j < len(runeB) {
-
 			if runeA[j] != runeB[j] {
 				al[runeA[j]] = append(al[runeA[j]], runeB[j])
 				continue outer
@@ -48,19 +61,65 @@ outer:
 	}
 	fmt.Println("]")
 
-	// Printing the adjacency list (al)
-	fmt.Print("al: map[")
-	for k, values := range al {
-		fmt.Printf("%c:[", k)
-		for i, v := range values {
-			fmt.Printf("%c", v)
-			if i < len(values)-1 {
-				fmt.Print(" ") // add a space between characters in the slice
+	//part II (topo sort the adj list)
+	visited := make(map[rune]struct{})
+	visitedSlice := make([]rune, 0)
+	visiting := make(map[rune]struct{})
+
+	var hasCycle func(rune) bool
+	hasCycle = func(pc rune) bool {
+		if _, exists := visited[pc]; exists {
+			return false
+		}
+
+		if _, exists := visiting[pc]; exists {
+			return true
+		}
+
+		visiting[pc] = struct{}{}
+
+		for _, neigh := range al[pc] {
+			if hasCycle(neigh) {
+				return true
 			}
 		}
-		fmt.Print("] ")
+
+		visitedSlice = append(visitedSlice, pc)
+		visited[pc] = struct{}{}
+		delete(visiting, pc)
+
+		return false
 	}
-	fmt.Println("]")
-	return ""
+
+	for pc := range set {
+		if hasCycle(pc) {
+			return ""
+		}
+	}
+
+	slices.Reverse(visitedSlice)
+
+	return string(visitedSlice)
 
 }
+
+// // Printing the set
+// fmt.Print("set: map[")
+// for k := range set {
+// 	fmt.Printf("%c:{} ", k)
+// }
+// fmt.Println("]")
+
+// // Printing the adjacency list (al)
+// fmt.Print("al: map[")
+// for k, values := range al {
+// 	fmt.Printf("%c:[", k)
+// 	for i, v := range values {
+// 		fmt.Printf("%c", v)
+// 		if i < len(values)-1 {
+// 			fmt.Print(" ") // add a space between characters in the slice
+// 		}
+// 	}
+// 	fmt.Print("] ")
+// }
+// fmt.Println("]")
--- alien-dictionary/alien-dict_test.go
+++ alien-dictionary/alien-dict_test.go
@@ -32,3 +32,59 @@ func TestAlienDict_Invalid(t *testing.T) {
 		t.Errorf("test did not pass")
 	}
 }
+
+func TestAlienDict_ZO(t *testing.T) {
+	words := []string{"z", "o"}
+	result := alienDict(words)
+	expected := "zo"
+
+	fmt.Println(result, expected)
+
+	if result == expected {
+		fmt.Println("test passed")
+	} else {
+		t.Errorf("test did not pass")
+	}
+}
+
+func TestAlienDict_hernf(t *testing.T) {
+	words := []string{"hrn", "hrf", "er", "enn", "rfnn"}
+	result := alienDict(words)
+	expected := "hernf"
+
+	fmt.Println(result, expected)
+
+	if result == expected {
+		fmt.Println("test passed")
+	} else {
+		t.Errorf("test did not pass")
+	}
+}
+
+func TestAlienDict_invalid_length(t *testing.T) {
+	words := []string{"abc", "ab"}
+	result := alienDict(words)
+	expected := ""
+
+	fmt.Println(result, expected)
+
+	if result == expected {
+		fmt.Println("test passed")
+	} else {
+		t.Errorf("test did not pass")
+	}
+}
+
+func TestAlienDict_z(t *testing.T) {
+	words := []string{"z"}
+	result := alienDict(words)
+	expected := "z"
+
+	fmt.Println(result, expected)
+
+	if result == expected {
+		fmt.Println("test passed")
+	} else {
+		t.Errorf("test did not pass")
+	}
+}
