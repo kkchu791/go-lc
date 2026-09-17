6fe026b63ab9454af6ef946a05be61f7f4346ba0
Kirk Chu <kkchu791@gmail.com>
2026-09-16 21:42:36 -0700

    add word ladder

--- word-ladder/word-ladder.go
+++ word-ladder/word-ladder.go
@@ -1,61 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strconv"
+)
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	//Part 1: How do you tell 2 words are different by one letter
+	if !slices.Contains(wordList, endWord) {
+		return 0
+	}
 
-	// first check lengths match, if they don't it can't work
-	// can add word1 chars to a set
-	// iterate through word 2 and count how many letters don't match
-	// if count ends up being 1, its a possible next Word
-
-	//len(word1) != len(word2)
-	//  immediately return 0
-	// initialize letter diff
-	//add word1 to a set
-	// iterater through word2
-	// if letter not in set
-	// count++
-
-	// if count == 1
-	// add it to words 1 al
-
-	wordList = append(wordList, beginWord)
+	if !slices.Contains(wordList, beginWord) {
+		wordList = append(wordList, beginWord)
+	}
 
 	al := make(map[string][]string, 0)
 	for _, word := range wordList {
 		al[word] = make([]string, 0)
 	}
 
-	for _, currWord := range wordList {
-		newSet := make(map[rune]struct{})
-		for _, r := range currWord {
-			newSet[r] = struct{}{}
-		}
-
-		for _, comparedWord := range wordList {
-			if comparedWord == currWord {
-				continue
-			}
+	for i := 0; i < len(wordList); i++ {
+		for j := 0; j < len(wordList); j++ {
+			word1 := wordList[i]
+			word2 := wordList[j]
 
 			count := 0
-			// iterate through the word
-			for _, r := range comparedWord {
-				if _, exists := newSet[r]; !exists {
+			k := 0
+
+			for k < len(word1) {
+				if word1[k] != word2[k] {
 					count++
+				}
+				k++
+
+				if count == 2 {
+					break
 				}
 			}
 
-			// if count is 1 add to our adj list
 			if count == 1 {
-				al[currWord] = append(al[currWord], comparedWord)
+				al[word1] = append(al[word1], word2)
 			}
 		}
 	}
 
-	fmt.Println(al, "al")
-	fmt.Println(endWord, "endWord")
+	// part II BFS Approach
+	// create a queue
+	// pop from queu
+	// explore neighbors
+	// until you find the endword
+	// add a secondary count helper with it
+
+	queue := make([][]string, 0)
+	visited := make(map[string]bool)
+	source := []string{
+		beginWord,
+		"1",
+	}
+
+	queue = append(queue, source)
+
+	for len(queue) > 0 {
+		currEl := queue[0]
+		queue = queue[1:]
+		word := currEl[0]
+		count := currEl[1]
+
+		if visited[word] {
+			continue
+		}
+
+		countNum, err := strconv.Atoi(count)
+		if err != nil {
+			fmt.Println("Error during conversion:", err)
+			return 0
+		}
+
+		if word == endWord {
+			return countNum
+		}
+
+		countNum = countNum + 1
+
+		for _, neigh := range al[word] {
+
+			str := strconv.Itoa(countNum)
+			el := []string{
+				neigh,
+				str,
+			}
+
+			queue = append(queue, el)
+		}
+
+		visited[word] = true
+	}
 
 	return 0
 
@@ -75,3 +115,63 @@ func main() {
 	}
 	fmt.Println(ladderLength(beginWord, endWord, wordList))
 }
+
+// part II DFS: Not working
+// visited := make(map[string]int)
+// visiting := make(map[string]bool)
+// var getMinPath func(string) int
+
+// getMinPath = func(word string) int {
+// 	if visiting[word] {
+// 		return 100
+// 	}
+
+// 	if val, exists := visited[word]; exists {
+// 		return val
+// 	}
+
+// 	//base case
+// 	if word == endWord {
+// 		return 1
+// 	}
+
+// 	visiting[word] = true
+
+// 	minPath := 100
+
+// 	for _, neigh := range al[word] {
+// 		minPath = min(minPath, getMinPath(neigh))
+// 	}
+
+// 	visited[word] = minPath
+// 	delete(visiting, word)
+// 	return minPath + 1
+// }
+
+// return getMinPath(beginWord)
+
+// first attempt at getting words
+
+// newSet := make(map[rune]struct{})
+// 		for _, r := range currWord {
+// 			newSet[r] = struct{}{}
+// 		}
+
+// 		for _, comparedWord := range wordList {
+// 			if comparedWord == currWord {
+// 				continue
+// 			}
+
+// 			count := 0
+// 			// iterate through the word
+// 			for _, r := range comparedWord {
+// 				if _, exists := newSet[r]; !exists {
+// 					count++
+// 				}
+// 			}
+
+// 			// if count is 1 add to our adj list
+// 			if count == 1 {
+// 				al[currWord] = append(al[currWord], comparedWord)
+// 			}
+// 		}
